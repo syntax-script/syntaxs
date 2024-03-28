6e4014fd89c261d87e8a44f34ce429067591eb93
efekos <hefeolgun10@gmail.com>
2024-03-28 19:07:13 +0300

    make log support non-string logs

--- src/log.ts
+++ src/log.ts
@@ -3,49 +3,49 @@ import { arg } from './argument/ArgResolver.js';
 
 export namespace log {
 
-    export function success(...message: string[]) {
+    export function success(...message: any[]) {
         message.forEach(m => console.log(`${chalk.whiteBright(chalk.bgGreen(' SUCCESS '))} ${m}`));
     }
 
-    export function error(...message: string[]) {
+    export function error(...message: any[]) {
         message.forEach(m => console.log(`${chalk.bgRed(' ERROR ')} ${m}`));
     }
 
-    export function raw(...message: string[]) {
+    export function raw(...message: any[]) {
         message.forEach(m => console.log(m));
     }
 
-    export function info(...message: string[]) {
+    export function info(...message: any[]) {
         message.forEach(m => console.log(`${chalk.bgBlue(' INFO ')} ${m}`));
     }
 
-    export function warn(...message: string[]) {
+    export function warn(...message: any[]) {
         message.forEach(m => console.log(`${chalk.whiteBright(chalk.bgYellow(' WARN '))} ${m}`));
     }
 
-    export function debug(...message: string[]) {
+    export function debug(...message: any[]) {
         if(arg.hasFlag('debug'))
-        message.forEach(m => console.log(`${chalk.whiteBright(chalk.bgYellowBright(' DEBUG '))} ${m}`));
+        message.forEach(m => console.log(`${chalk.whiteBright(chalk.bgYellowBright(' DEBUG '))}`,m));
     }
 
-    export function notice(...message: string[]) {
+    export function notice(...message: any[]) {
         message.forEach(m => console.log(`${chalk.whiteBright(chalk.bgBlue(' NOTICE '))} ${m}`));
     }
 
     export namespace exit {
-        export function error(...message: string[]) {
+        export function error(...message: any[]) {
             log.error(...message);
             log.raw('', '', process.cwd(), 'syntaxs@0.0.1-alpha');
             process.exit(1);
         }
 
-        export function raw(...message: string[]) {
+        export function raw(...message: any[]) {
             log.raw(...message);
             log.raw('', '', process.cwd(), 'syntaxs@0.0.1-alpha');
             process.exit(0);
         }
 
-        export function success(...message: string[]) {
+        export function success(...message: any[]) {
             log.success(...message);
             log.raw('', '', process.cwd(), 'syntaxs@0.0.1-alpha');
             process.exit(0);
