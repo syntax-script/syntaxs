fe6098ff9779a3550d4635d05c5f2fb86b76a55b
efekos <hefeolgun10@gmail.com>
2024-03-28 20:05:02 +0300

    add timer module

---
+++ src/util/timer.ts
@@ -0,0 +1,22 @@
+export namespace timer {
+    
+    var startTime:number;
+    const markers:Record<string,number> = {};
+
+    export function start(){
+        startTime = Date.now();
+    }
+
+    export function sinceStart():number{
+        return Date.now()-startTime;
+    }
+
+    export function sinceMarker(marker:string):number{
+        return Date.now()-(markers[marker]??startTime);
+    }
+
+    export function mark(marker:string):void {
+        markers[marker] = Date.now();
+    }
+
+}
