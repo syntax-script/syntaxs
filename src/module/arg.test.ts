445f81a8becb4eb8d4562ddfbc376a3ddd5f2372
efekos <hefeolgun10@gmail.com>
2024-04-17 17:08:05 +0300

    remove tests (fuck jest fuck everything im literally done its been 10 years since es came out and they still dont support it fuck this shit)

--- src/module/arg.test.ts
+++
@@ -1,9 +0,0 @@
-import {expect,test} from '@jest/globals';
-import { arg } from './arg.js';
-
-test('Argument resolver should work.',()=>{
-    const args = arg.resolve(['help','--watch','-version','1.0']);
-    expect(arg.getCommand()).toBe('hello');
-    expect(arg.hasFlag('watch')).toBe(true);
-    expect(arg.getArgument('version')).toBe('1.0');
-});
