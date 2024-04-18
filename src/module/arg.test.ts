1b904fc065426b7141861f6df5b2e06a23791aef
efekos <hefeolgun10@gmail.com>
2024-04-18 08:23:40 +0300

    Create a  test using a local test runner (i literally made my own test runner in a day this is how much i hated jest in one day omfg i still hate that shit i hope i dont see it again)

---
+++ src/module/arg.test.ts
@@ -0,0 +1,22 @@
+import { describe, it } from 'es-test/lib/testRunner.js';
+import { arg } from './arg.js';
+import { expect } from 'chai';
+
+describe('Argument handler', () => {
+
+    arg.resolve(['help', '--watch', '-arg', 'yes']);
+
+    it('should parse commands correctly', () => {
+        expect(arg.getCommand()).to.equal('help');
+    });
+
+    it('should parse flags correctly', () => {
+        expect(arg.getFlags()).to.include('watch');
+        expect(arg.hasFlag('watch')).to.equal(true);
+    });
+
+    it('should parse arguments correctly', () => {
+        expect(arg.getArgument('arg')).to.equal('yes');
+    });
+
+});
